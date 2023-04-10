0ac333a9d2152b8ee014051d5a7247dda6df723b
Utku Kaan Gülsoy <47040490+utkugulsoy@users.noreply.github.com>
2023-04-10 16:48:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/greetings/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.greetings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button btn;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+        btn = findViewById(R.id.btn);
+        editText = findViewById(R.id.editText1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "Hello " + name , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
